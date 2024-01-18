a3d0aa2332c8a0b926a5700b30beab95adf69c00
Sujay Mondal <sujay.m6160@gmail.com>
2024-01-18 14:15:55 +0600

    Authentication & Authorization

---
+++ eighth_project/first_app/views.py
@@ -0,0 +1,122 @@
+from django.shortcuts import render, redirect
+from .forms import RegisterForm, ChangeUserData
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, SetPasswordForm
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+# Create your views here.
+
+
+def home(request):
+    return render(request, './home.html')
+
+
+def signup(request):
+    if not request.user.is_authenticated:
+        if request.method == 'POST':
+            form = RegisterForm(request.POST)
+            if form.is_valid():
+                messages.success(request, 'Account created successfully')
+                form.save()
+                print(form.cleaned_data)
+        else:
+            form = RegisterForm()
+        return render(request, './signup.html', {'form': form})
+    else:
+        return redirect('profile')
+
+
+def user_login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['username']
+            userpass = form.cleaned_data['password']
+            # check kortechi user database e ache kina
+            user = authenticate(username=name, password=userpass)
+            if user is not None:
+                login(request, user)
+                return redirect('profile')  # profile page e redirect korbe
+            
+    else:
+        form = AuthenticationForm()
+    return render(request, './login.html', {'form': form})
+
+
+def profile(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = ChangeUserData(request.POST, instance=request.user)
+            if form.is_valid():
+                messages.success(request, 'Account updated successfully')
+                form.save()
+        else:
+            form = ChangeUserData(instance=request.user)
+        return render(request, './profile.html', {'form': form})
+    else:
+        return redirect('signup')
+
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+def pass_change(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = PasswordChangeForm(user=request.user, data=request.POST)
+            if form.is_valid():
+                form.save()
+                # password update korbe
+                update_session_auth_hash(request, form.user)
+                return redirect('profile')
+        else:
+            form = PasswordChangeForm(user=request.user)
+        return render(request, './passchange.html', {'form': form})
+    else:
+        return redirect('login')
+
+
+def pass_change2(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = SetPasswordForm(user=request.user, data=request.POST)
+            if form.is_valid():
+                form.save()
+                # password update korbe
+                update_session_auth_hash(request, form.user)
+                return redirect('profile')
+        else:
+            form = SetPasswordForm(user=request.user)
+        return render(request, './passchange.html', {'form': form})
+    else:
+        return redirect('login')
+
+
+def change_user_data(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            form = ChangeUserData(request.POST, instance=request.user)
+            if form.is_valid():
+                messages.success(request, 'Account updated successfully')
+                form.save()
+                print(form.cleaned_data)
+        else:
+            form = ChangeUserData()
+        return render(request, './profile.html', {'form': form})
+    else:
+        return redirect('signup')
+
+
+
+# Method 2: Using request.user.is_authenticated
+# Another way to check if user is logged in is by using the is_authenticated attribute of the request.user object.
+
+# def some_view(request):
+#     if request.user.is_authenticated:
+#         # The user is logged in
+#         # Perform actions for authenticated users
+#     else:
+#         # The user is not logged in
+#         # Perform actions for unauthenticated users
+# In this method, you can explicitly check if the is_authenticated attribute is True to determine whether the user is logged in.
