ad4eed44b84c027045708b88f08dfbcb5ca36637
Sujay Mondal <sujay.m6160@gmail.com>
2024-01-20 19:33:39 +0600

    django codes

---
+++ Blog_Project_Part_2/blog_project/posts/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from categories.models import Category
+from django.contrib.auth.models import User
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    category = models.ManyToManyField(Category) # ekta post multiple categorir moddhe thakte pare abar ekta categorir moddhe multiple post thakte pare
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title 
+    
+
+# https://richardgingras.medium.com/
+# Author (One-to-Many with Posts): An author can have multiple blog posts.
+# Category (Many-to-Many with Posts): A blog post can belong to multiple categories.
+# Profile (One-to-One with User): Each user has a single profile.
+   
+    
